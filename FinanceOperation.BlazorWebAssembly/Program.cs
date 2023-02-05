1517c416a69cc367cf6c258d7ec7ad617ebbc8ff
Maksym Konotop <konotop2001@gmail.com>
2023-02-05 14:25:16 +0200

    Add blazor web assembly project and update docker files

---
+++ FinanceOperation.BlazorWebAssembly/Program.cs
@@ -0,0 +1,13 @@
+﻿using FinanceOperation.BlazorWebAssembly;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+var configuration = builder.Configuration;
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(configuration.GetValue<string>("BaseUrl")!) });
+
+await builder.Build().RunAsync();
